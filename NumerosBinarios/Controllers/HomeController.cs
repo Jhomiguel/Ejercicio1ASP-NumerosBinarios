2f8e1421339d4c2c85a3e5a3584127a0d55a9307
Jhomiguel <jhomiguelflorianmendez20@gmail.com>
2020-03-01 11:04:32 -0400

    Version 1

---
+++ NumerosBinarios/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NumerosBinarios.Models;
+
+namespace NumerosBinarios.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new NumBinario());
+        }
+        [HttpPost]
+        public IActionResult Index(NumBinario Nums)
+        {
+            if (ModelState.IsValid)
+            {
+                Nums.NBinarios = NumBinario.Binario(Nums.Numeros);
+                return View("Mostrar", Nums);
+            }
+            return View(Nums);
+        }
+    }
+}
---
+++ NumerosBinarios/Models/NumBinario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NumerosBinarios.Models
+{
+    public class NumBinario
+    {
+        public List<int> Numeros { get; set; }
+        public List<string> NBinarios { get; set; }
+        public static List<string>Binario(IEnumerable<int> Nums)
+        {
+            List<string> BNum = new List<string>();
+            foreach (int num in Nums)
+            {
+                string binario = Convert.ToString(num, 2);
+                while (binario.Length < 8)
+                {
+                    binario = 0 + binario;
+                }
+                BNum.Add(binario);
+            }
+            return BNum;
+        }
+    }
+}
